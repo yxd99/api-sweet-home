7883c4e57db4205214c31e46025c46ac09713e23
Yesid Hernandez <fyesid.h@gmail.com>
2023-09-29 21:22:00 -0500

    security implemented

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 (async function () {
   const port = process.env.PORT ?? 3000;
-  const app = await NestFactory.create(AppModule);
+  const prefix = process.env.PREFIX ?? 'api';
+
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Api My Home')
     .setDescription('Api develop for the project my-home')
     .setVersion('1')
     .build();
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, swaggerDocument);
-  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  SwaggerModule.setup(prefix, app, swaggerDocument);
+  app.setGlobalPrefix(prefix);
   await app.listen(port, async () => {
     console.log(`App in: ${await app.getUrl()}`);
   });
